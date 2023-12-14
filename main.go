3711b28aac37d55aa98a74420c6bdf374b8aacd1
changpengg <changpeng94@gmai.com>
2023-12-14 11:00:41 +0800

    use actions container

--- main.go
+++ main.go
@@ -1,40 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	// route
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
-	})
+	route := mux.NewRouter()
 
-	// favicon.ico
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./favicon.ico")
-	})
+	route.HandleFunc("/", Index).Methods("GET")
+	route.HandleFunc("/health", HealthCheck).Methods("GET")
 
-	// 404
-	http.HandleFunc("/404", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./404.html")
-	})
+	log.Println("Server started on: http://localhost:8080")
 
-	// 500
-	http.HandleFunc("/500", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./500.html")
-	})
+	log.Fatal(http.ListenAndServe(":8080", route))
+}
 
-  // post mothon create post
-  http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "post")
-  })
+func Index(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Hello World"))
+}
 
-  log.Println("Server is running on port http://localhost:8080")
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+// HealthCheck is a function to check the health of the application
+func HealthCheck(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
 }
